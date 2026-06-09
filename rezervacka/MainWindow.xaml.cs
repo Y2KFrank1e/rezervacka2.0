8c4047fa8a81dd5c5c9a7eb85065ae82aad13825
Jan Frank <jan.frank@infis.cz>
2026-06-09 10:01:33 +0200

    Přidejte soubory projektu.

---
+++ rezervacka/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace rezervaceKina
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+        private void btnReservation_Click(object sender, RoutedEventArgs e)
+        {
+            ComboBoxItem selectedFilm = (ComboBoxItem)cmbFilms.SelectedItem;
+            if (selectedFilm == null)
+            {
+                MessageBox.Show("Please select a film.");
+                return;
+            }
+
+            string filmName = selectedFilm.Content.ToString();
+            if (rbPremium.IsChecked == true)
+            {
+
+            }
+            else
+            {
+
+            }
+        }
+    }
+}
